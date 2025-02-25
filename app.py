87728fa6fddd4ac0bcaa6fbfb60514281a2e95ce
Sandeep K <sandeepkumaresan4676@gmail.com>
2025-02-25 09:38:37 +0530

    first

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+# MongoDB Configuration
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+client = MongoClient(MONGO_URI)
+db = client.get_database("testdb")
+collection = db.get_collection("items")
+
+@app.route('/')
+def home():
+    return "Flask App with MongoDB on Azure!"
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = list(collection.find({}, {"_id": 0}))
+    return jsonify(items)
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    data = request.json
+    collection.insert_one(data)
+    return jsonify({"message": "Item added!"}), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
